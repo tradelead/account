253dc2afdb428d0eaa8e777f5f6c6913638a25f1
calebmcelroy <calebmcelroy@gmail.com>
2019-06-28 22:17:50 -0400

    force .env to override vars set on os for integration tests.

--- tests/integration/env-setup.js
+++ tests/integration/env-setup.js
@@ -1,4 +1,12 @@
-require('dotenv').config({ path: require.resolve('./.env') });
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+
+const envConfig = dotenv.parse(fs.readFileSync(path.resolve(__dirname, './.env')));
+Object.keys(envConfig).forEach((k) => {
+  process.env[k] = envConfig[k];
+});
+
 const flushDBs = require('./flushDBs');
 
 beforeAll(async () => {
